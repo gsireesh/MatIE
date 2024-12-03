1f63f0f5d48a4f04387ff1e0bb2143033f6616e8
Sireesh K Gururaja <sgururaj@wqs.cmu.edu>
2024-12-03 15:02:52 -0500

    MatIE Service working

--- matie_service.py
+++ matie_service.py
@@ -1,19 +1,25 @@
-from dataclasses import dataclass
+# from dataclasses import dataclass
+import logging
 import os
 import re
 import subprocess
+import sys
 from tempfile import TemporaryDirectory
-from typing import List
+from typing import Dict, List, Union
 
 import fastapi
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pydantic.dataclasses import dataclass
 
+NER_MODEL_DIR = "model"
+VOCAB_DIR = "vpack_mat"
+DECODE_SCRIPT = "./decode.sh"
+WORKING_DIRECTORY = "matie_annotation"
 
-app = FastAPI
 
 @dataclass
-class MatIEEntity(BaseModel):
+class MatIEEntity:
     id: str
     entity_type: str
     start: int
@@ -21,27 +27,40 @@ class MatIEEntity(BaseModel):
     entity_string: str
 
 
-relation_re = re.compile("R\d+\t(?P<r_type>.*) Arg1:(?P<arg1>T\d+) Arg2:(?P<arg2>T\d+)")
+@dataclass
+class MatIERelation:
+    id: str
+    relation_type: str
+    arg1: str
+    arg2: str
 
 
-def generate_txt(self, working_folder, paragraph_text, key):
+MatIEResponse = Dict[str, Dict[str, Union[str, List[MatIEEntity], List[MatIERelation]]]]
+
+
+relation_re = re.compile(r"R(?P<r_id>\d+)\t(?P<r_type>.*) Arg1:(?P<arg1>T\d+) Arg2:(?P<arg2>T\d+)")
+
+
+def generate_txt(tmp_folder, paragraph_text, key):
     file_path = os.path.join(
-        working_folder, f"{key}.txt".replace("/", "_")
+        tmp_folder, f"{key}.txt".replace("/", "_")
     )
 
-    os.makedirs(working_folder, exists_ok=True)
+    os.makedirs(tmp_folder, exist_ok=True)
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(paragraph_text)
 
     return paragraph_text
 
 
-def run_matIE(working_folder):
+def run_matie(working_folder):
+
     for dir_name in os.listdir(working_folder):
         if (
             not os.path.isdir(os.path.join(working_folder, dir_name))
             or "_original" in dir_name
         ):
+            logger.error(f"{os.path.join(working_folder, dir_name)} not found!")
             continue
 
         dir_path = os.path.join(working_folder, dir_name)
@@ -53,27 +72,30 @@ def run_matIE(working_folder):
             output_folder,
         )
 
-def process_files_multiprocess(self, input_folder, output_folder):
+
+def process_files_multiprocess(input_folder, output_folder):
     env_vars = os.environ.copy()
-    env_vars["MODEL_DIR"] = self.NER_model_dir
-    env_vars["VOCAB_DIR"] = self.vocab_dir
+    env_vars["MODEL_DIR"] = NER_MODEL_DIR
+    env_vars["VOCAB_DIR"] = VOCAB_DIR
     env_vars["CUDA_VISIBLE_DEVICES"] = ""  # needs to fix later
     # bizarrely, taking out the `../ht-max` from these paths breaks something in MatIE
     env_vars["INPUT_DIR"] = os.path.join("./", input_folder.replace("//", "/"))
     env_vars["OUTPUT_DIR"] = os.path.join("./", output_folder.replace("//", "/"))
     env_vars["EXTRA_ARGS"] = ""
 
-    subprocess.run(["chmod", "+x", self.decode_script], check=True)
+    subprocess.run(["chmod", "+x", DECODE_SCRIPT], check=True)
     try:
+        logger.info("Running MatIE subprocess...")
         output = subprocess.check_output(
-            self.decode_script,
+            DECODE_SCRIPT,
             stderr=subprocess.STDOUT,
             shell=True,
             env=env_vars,
-            cwd=self.matIE_directory,
         )
+        logger.info("Done.")
     except subprocess.CalledProcessError as e:
-        print("Status : FAIL", e.returncode, e.output)
+        logger.error("Status : FAIL", e.returncode, e.output)
+
 
 def parse_ann_content(ann_content):
     entities = []
@@ -88,31 +110,43 @@ def parse_ann_content(ann_content):
             entities.append(entity)
         elif line.startswith("R"):
             match = relation_re.fullmatch(line)
-            relations.append(
-                {
-                    "relation_type": match.group("r_type"),
-                    "arg1": match.group("arg1"),
-                    "arg2": match.group("arg2"),
-                }
-            )
+            relations.append(MatIERelation(
+                id=match.group("r_id"),
+                relation_type=match.group("r_type"),
+                arg1=match.group("arg1"),
+                arg2=match.group("arg2"),
+            ))
 
     return {"entities": entities, "relations": relations}
 
 
-app.get("/")
+app = FastAPI()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# StreamHandler for the console
+stream_handler = logging.StreamHandler(sys.stdout)
+log_formatter = logging.Formatter("%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s")
+stream_handler.setFormatter(log_formatter)
+logger.addHandler(stream_handler)
+
+@app.get("/")
 def get_root():
     return {"hello": "world"}
+
 
 @app.get("/status")
 def get_status():
     return "Service is up!"
 
-@app.post("/annotate_strings")
-def annotate_strings(key_to_string: dict[str, str]) -> dict[str, dict[str, dict[str, str] | list[
-    MatIEEntity]]]:
-    print("Creating temporary input files")
 
-    doc_temp_folder = TemporaryDirectory(prefix="matie_file_annotation_")
+@app.post("/annotate_strings", response_model=MatIEResponse)
+def annotate_strings(key_to_string: Dict[str, str]) -> MatIEResponse:
+    logger.info("Creating temporary input files")
+
+    os.makedirs(WORKING_DIRECTORY, exist_ok=True)
+    doc_temp_folder = TemporaryDirectory(dir=WORKING_DIRECTORY, prefix="matie_file_annotation_")
 
     input_paragraphs = {}
 
@@ -120,10 +154,10 @@ def annotate_strings(key_to_string: dict[str, str]) -> dict[str, dict[str, dict[
         paragraph_text = paragraph.replace("\n", " ")
         input_paragraphs[key] = generate_txt(doc_temp_folder.name, paragraph_text, key)
 
-    print("Annotating temp files")
-    run_matIE()
+    logger.info("Annotating temp files")
+    run_matie(working_folder=WORKING_DIRECTORY)
 
-    print("Formatting annotated files...")
+    logger.info("Formatting annotated files...")
 
     return_content = dict()
     for key in input_paragraphs:
@@ -136,10 +170,8 @@ def annotate_strings(key_to_string: dict[str, str]) -> dict[str, dict[str, dict[
                 os.path.join(folder_name, f"{key}.txt".replace("/", "_"))
         ) as f:
             annotated_text = f.read()
-            annotated_content["text"]  = annotated_text
+            annotated_content["text"] = annotated_text
         return_content[key] = annotated_content
 
+    logger.info(return_content)
     return return_content
-
-
-
